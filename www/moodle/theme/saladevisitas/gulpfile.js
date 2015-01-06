78757f001de5aced7c48f5aec2a3e2de2358fec7
felisio <fabio.elisio@gmail.com>
2015-01-06 14:53:43 -0300

    criado estrtura less
    criado estrtura less

--- www/moodle/theme/saladevisitas/gulpfile.js
+++ www/moodle/theme/saladevisitas/gulpfile.js
@@ -13,10 +13,10 @@ var shell = require('gulp-shell');
 // load plugins
 var $ = require('gulp-load-plugins')();
 
-var configfile = path.join( path.dirname(path.dirname(__dirname)), 'config.php');
+//var configfile = path.join( path.dirname(path.dirname(__dirname)), 'config.php');
 
 gulp.task('cmd', shell.task([
-  'php -r define(\'CLI_SCRIPT\', true); require(\'' + configfile  + '\'); theme_reset_all_caches();'
+  'php -r "define(\'CLI_SCRIPT\', true); require(\'/var/www/moodle/config.php\'); theme_reset_all_caches();"'
 ]));
 
 gulp.task('less', function() {
