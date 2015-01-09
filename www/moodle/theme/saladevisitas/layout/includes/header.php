2e0929078e42f5b349af9d88cac121b05e1bd62d
thiagoleite86 <thiago.leite@nad1.com.br>
2015-01-09 17:40:47 -0300

    alterações de estilo
    alterações de estilo

--- www/moodle/theme/saladevisitas/layout/includes/header.php
+++ www/moodle/theme/saladevisitas/layout/includes/header.php
@@ -37,6 +37,7 @@ echo $OUTPUT->doctype() ?>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="<?php echo $OUTPUT->pix_url('favicon', 'theme')?>" />
+    <link href='http://fonts.googleapis.com/css?family=Roboto:500,300,400' rel='stylesheet' type='text/css'>
     <?php echo $OUTPUT->standard_head_html() ?>
 </head>
 
