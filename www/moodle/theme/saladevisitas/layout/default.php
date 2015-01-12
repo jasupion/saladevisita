825fb7f3480de5f5a0bc0a224bd8734bc36ccf65
Marcus Vinicius <marcusvinicius30798@gmail.com>
2015-01-12 15:08:43 -0300

    Ajustado botão close
    Ajustado botão close

--- www/moodle/theme/saladevisitas/layout/default.php
+++ www/moodle/theme/saladevisitas/layout/default.php
@@ -31,6 +31,7 @@
 <?php if ($hassidepre) { ?>
   <div id="region-pre" class="block-region">                    
         <div class="nav">
+          <div class="menu-anchor menu-close"><i class="fa fa-times"></i></div>
            <div class="region-content">
                 <?php echo $OUTPUT->blocks_for_region('side-pre') ?>
                 <?php 
