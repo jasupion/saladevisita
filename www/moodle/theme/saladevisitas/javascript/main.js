28750526d08f9d8cfeb3b4ee283892474c1f3a4c
JaderGJ <jadergomes20@gmail.com>
2015-05-15 17:30:56 -0300

    Remoção do Overlay na tela de login

--- www/moodle/theme/saladevisitas/javascript/main.js
+++ www/moodle/theme/saladevisitas/javascript/main.js
@@ -1,8 +1,8 @@
 // scripts criado para o projeto
 (function(window, document,$){
-  'use strict';        
+  'use strict';
   var App = (function(){
-    
+
     var _public = {};
 
     _public.pathSvg = function(){
@@ -11,47 +11,47 @@
 	        imgID = $img.attr('id'),
 	        imgClass = $img.attr('class'),
 	        imgURL = $img.attr('src');
-	    
+
 	        jQuery.get(imgURL, function(data) {
 	            var $svg = jQuery(data).find('svg');
-	    		
+
 	    		if(typeof imgID !== 'undefined') {
 	                $svg = $svg.attr('id', imgID);
 	            }
-	            
+
 	            if(typeof imgClass !== 'undefined') {
 	                $svg = $svg.attr('class', imgClass+' replaced-svg');
 	            }
-	    
+
 	            $svg = $svg.removeAttr('xmlns:a');
-	    
+
 	            $img.replaceWith($svg);
-	    
+
 	        }, 'xml');
-	    
+
 	    });
     };
 
     _public.menuSandwich = function(){
-    	// if($('.tree_item').length > 0){
+     if($('.tree_item').length > 0){
 			$('.menu-anchor').on('click touchstart', function(e){
 					$('html').toggleClass('menu-active');
-				  	$('.overlay').show();		  	
+				  	$('.overlay').show();
 				  	e.preventDefault();
 
-				});	
+				});
 
 				$('.overlay').on('click',function(e){
-		                $('.overlay').hide();                                
+		                $('.overlay').hide();
 		                $('html').toggleClass('menu-active');
 				  		e.preventDefault();
 		            });
-		// }else{
-		// 	$('.menu-anchor').hide();
-		// } 
+		 }else{
+		 	   $('.menu-anchor').hide();
+		 }
     };
 
-     _public.init = function(){
+    _public.init = function(){
 		_public.pathSvg();
 		_public.menuSandwich();
 		$(".dock_left_vertical nothingdocked").remove();
@@ -59,13 +59,9 @@
 		$("fieldset").removeClass("hidden");
     };
 
-    return _public; 
+    return _public;
 
   })();
   window.App = App;
   App.init();
 })(window, document,$);
-
-
-
-
